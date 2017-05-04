2013c656a9bf8b07eb478e88b80a0a6f0456c27c
Rachel H Rotteveel <rachelro@mit.edu>
2017-05-04 17:03:12 -0400

    failing at problem 1

--- ps8.scm
+++ ps8.scm
@@ -13,7 +13,7 @@ May 5, 2017
 (define-cell john-earnings)
 (tell! john-earnings (make-interval 20 27) 'Harry-estimate)
 
-(content john-earnings)]
+(content john-earnings)
 #|
 #(tms (#(value=#[interval 20 27],
 	       premises=(harry-estimate),
@@ -215,6 +215,92 @@ and new intervals can be placed in them.
 ;;; Problem 8.1 ;;;
 ;;;;;;;;;;;;;;;;;;;
 
+(initialize-scheduler)    
+
+
+(define (create-people people)
+  (for-each
+   (lambda (person)
+     (display person)
+     (define-cell person)
+     ;; add properties to person cell
+     (define-cell height)
+     (eq-put! person 'height height)
+     (define-cell weight)
+     (eq-put! person 'weight weight)
+     (define-cell income)
+     (eq-put! person 'income income)
+     (define-cell expenses)
+     (eq-put! person 'expenses expenses)) 
+   people))
+
+
+#|
+(define (create-people people)
+  (display people)
+  (for-each 
+   (lambda (p)
+     (display p)
+     (define-cell p)
+     (let-cells (height-cell weight-cell income-cell expenses-cell)  
+                ((eq-put! p 'height height-cell)
+                 (eq-put! p 'weight weight-cell)
+                 (eq-put! p 'income income-cell)
+                 (eq-put! p 'expenses expenses-cell))))
+   people))
+|#
+               
+(create-people '('martin 'joey 'melinda 'barb 'rob))
+(tell! (eq-get 'joey 'height) 56 'melinda)
+
+
+(define (add-symbolic-ranges name height weight income expenses) 
+  (eq-put! name 'height height)
+  ((c:bins
+    (named-ranges
+     name
+     `(short ,(make-interval 0 100))
+     `(average ,(make-interval 100 150))
+     `(tall ,(make-interval 150 250))))
+   height)
+  
+  (eq-put! name 'weight weight)
+  ((c:bins
+    (named-ranges
+     name
+     `(skinny ,(make-interval 0 100))
+     `(average ,(make-interval 100 150))
+     `(large ,(make-interval 150 250))
+     `(obese ,(make-interval 250 500))))
+   weight)
+  
+  (eq-put! name 'income income)
+  ((c:bins
+    (named-ranges
+     name
+     `(poor ,(make-interval 0 20000))
+     `(middle-class ,(make-interval 20000 80000))
+     `(upper-middle-class ,(make-interval 80000 120000))
+     `(wealthy ,(make-interval 120000 1000000))))
+   income)
+  
+  (eq-put! name 'expenses expenses)
+  ((c:bins
+    (named-ranges
+     name
+     `(frugal ,(make-interval 0 20000))
+     `(average ,(make-interval 20000 50000))
+     `(big-spender ,(make-interval 50000 300000))))
+   expenses))
+
+(define-cell 'martin)
+(tell! (eq-get 'martin 'height) 185 'barb)
+(add-symbolic-ranges Martin 120 180 20000 50000)
+(inquire Martin)
+
+
+
+(content martin)
 
 
 
